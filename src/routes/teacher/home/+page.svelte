36be3c46e6de5ec1d7a0659a2ec58b3d006ea0b4
Jamie1111 <ever0067@hz.nl>
2025-11-27 11:44:30 +0100

    Map import functie

--- src/routes/teacher/home/+page.svelte
+++ src/routes/teacher/home/+page.svelte
@@ -1,8 +1,7 @@
-<div class="bg-white rounded-xl shadow p-4">
-  <h1 class="text-xl font-bold mb-4">Jouw Schoolplein</h1>
+<script>
+  import Map from '$lib/components/Map.svelte';
+</script>
 
-  <div class="border rounded-xl overflow-hidden h-80 bg-gray-200 flex items-center justify-center">
-    <!-- Replace with real map -->
-    <p class="opacity-70">[ Map Placeholder ]</p>
-  </div>
-</div>
+<p class="text-2xl font-bold mb-4">Jouw Schoolplein</p>
+
+<Map title="Plaats hier jouw schoolplein!"/>
