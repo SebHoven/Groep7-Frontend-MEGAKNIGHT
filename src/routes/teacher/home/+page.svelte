a7a86d3e52b3afcfef1789232c6e155a07063fee
Jero <exte0005@hz.nl>
2025-12-01 11:32:44 +0100

    frontend create functionality

--- src/routes/teacher/home/+page.svelte
+++ src/routes/teacher/home/+page.svelte
@@ -2,6 +2,10 @@
   import Map from '$lib/components/Map.svelte';
 </script>
 
+<a href="/teacher/task" class="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded float-right">
+  Create Task
+</a>
+
 <p class="text-2xl font-bold mb-4">Jouw Schoolplein</p>
 
 <Map title="Plaats hier jouw schoolplein!"/>
