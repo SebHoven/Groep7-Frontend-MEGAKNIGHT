37e8c62efc30546353895fc64b237973259b2641
sebmesterr <hove0021@hz.nl>
2026-01-18 12:57:52 +0100

    Pipeline (2.4 + 3.1) and Testing (2.2)
    Ik heb de pipeline aangepast zodat docker wordt gebruikt en de env niet meer direct in de pipeline wordt omschreven. Ook heb ik een linter toegevoegd.

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import js from "@eslint/js";
+import svelte from "eslint-plugin-svelte";
+
+export default [
+  js.configs.recommended,
+
+  ...svelte.configs["flat/recommended"],
+
+  {
+    files: ["**/*.svelte"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    }
+  }
+];
